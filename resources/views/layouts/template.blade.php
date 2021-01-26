16cb0255284bb408ba35108477b1fae55a01db12
f458f430 <76420887+f458f430@users.noreply.github.com>
2021-01-26 17:00:08 +0800

    checkout

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>樣板</title>
+     <!-- CSRF Token -->
+     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- fontawesome -->
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <!-- bootstrap -->
