debffa95ef5686ec4446e4070bb3c172570aaa44
mason <xllin11@outlook.com>
2021-01-27 10:04:57 +0800

    temp OK

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>樣板</title>
+    <title>溫事</title>
      <!-- CSRF Token -->
      <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- fontawesome -->
