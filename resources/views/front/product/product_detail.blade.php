64ca21b721eb2c23551a4ec0fc9f54d65695f546
mason <xllin11@outlook.com>
2021-01-27 12:06:24 +0800

    clear

--- resources/views/front/product/product_detail.blade.php
+++ resources/views/front/product/product_detail.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.template')
 
 @section('css')
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- swiper -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     <link rel="stylesheet" href="{{ asset('css/04-product_detail.css') }}">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
--- resources/views/front/product/product_index.blade.php
+++ resources/views/front/product/product_index.blade.php
@@ -1,18 +1,5 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>商品目錄</title>
-    <link rel="stylesheet" href="/css/04-product_index.css">
-</head>
-
-
-<body> --}}
-
 @extends('layouts.template')
-{{-- <title>商品目錄</title> --}}
+
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/04-product_index.css')}}">
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
